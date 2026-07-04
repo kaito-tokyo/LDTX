7228b119a2195770bee479fcf364c0e3dd87561d
Kaito Udagawa <umireon@kaito.tokyo>
2026-07-04 19:41:48 +0900

    Add ci_scripts for Xcode Cloud
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,11 @@
+#!/bin/dash
+
+# SPDX-FileCopyrightText: 2026 Kaito Udagawa <umireon@kaito.tokyo>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+if ! command -v xcodegen >/dev/null 2>&1; then
+  brew install xcodegen
+fi
+
+xcodegen generate
