38d1c896f5681a3b8f45db9608d3aa4a459230e6
Kaito Udagawa <umireon@kaito.tokyo>
2026-08-09 17:06:35 +0900

    Update ci_post_clone.sh
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,6 +6,12 @@
 
 set -eu
 
+# Xcode Cloud can validate SwiftPM build tool plugins while preparing package
+# dependencies. mlx-swift includes the CudaBuild plugin, so allow package
+# plugins before dependency resolution and project generation begin.
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
+defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidation -bool YES
+
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_ENV_HINTS=1
