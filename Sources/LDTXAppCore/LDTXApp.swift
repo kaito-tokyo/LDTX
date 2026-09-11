8dd1ea44afda6f0b553a16071ace79fa114a6315
Kaito Udagawa <umireon@kaito.tokyo>
2026-09-12 00:03:12 +0900

    Fix CI after app consolidation
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- Sources/LDTXAppCore/LDTXApp.swift
+++ Sources/LDTXAppCore/LDTXApp.swift
@@ -2,7 +2,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import AppKit
-import LDTXRecordPlayerUI
 import LDTXRecording
 import LDTXWorkspace
 import LDTXYouTubeAuth
--- Tests/LDTXRecordPlayerUIEasyTests/RecordingMarkerStoreTests.swift
+++ Tests/LDTXRecordPlayerUIEasyTests/RecordingMarkerStoreTests.swift
@@ -6,7 +6,7 @@
 import Foundation
 import Testing
 
-@testable import LDTXRecordPlayerUI
+@testable import LDTXApp
 
 @Suite
 struct RecordingMarkerStoreIntegrationTestSuite {
